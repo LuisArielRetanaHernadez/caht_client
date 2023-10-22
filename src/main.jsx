fe2f516fc75943b854bb73ce2648c24ad745932a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-10-21 20:28:37 -0600

    configurando el las rutas del router-dom con la rutas / y /login

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
 // redux
 import { Provider } from 'react-redux'
 import { store } from './app/store.js'
 
+
+// router-dom
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+// Pages
+import Home from './Pages/Home/Home.page.jsx'
+import Login from './Pages/Login/Login.pages.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    index: true
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
