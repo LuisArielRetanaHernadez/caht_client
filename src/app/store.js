90495128802e349c6b4764f2d120f12213eb8b4c
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-10-21 20:23:34 -0600

    corregiendo la importacion del userSlice y instalando el paquete react-redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 // slices
-import { userSlice } from "../features/user/userSlice";
+import userSlice from "../features/user/userSlice";
 
 export const store = configureStore({
     reducer: {
