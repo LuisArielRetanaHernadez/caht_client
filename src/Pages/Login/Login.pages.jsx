ac933548069127df2c7854179f6ec0b356a6db85
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-10-21 20:22:38 -0600

    obtiendo los datos de los inputs y autentificarse con la api

--- src/Pages/Login/Login.pages.jsx
+++ src/Pages/Login/Login.pages.jsx
@@ -1,9 +1,51 @@
+import { useState } from "react"
+
+// redux
+import { useDispatch } from "react-redux"
+import { loginAsync } from "../../features/user/userSlice"
 
 const Login = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  })
+  const dispatch = useDispatch()
+  
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    })
+  }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(loginAsync(values))
+    setValues({
+      email: "",
+      password: "",
+    })
+
+  }
+  
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Correo</label>
+          <input
+           name="email"
+           type="email" />
+           onChange={handleChange}
+        </div>
+        <div>
+          <label>Contraseña</label>
+          <input 
+            name="password"
+            type="password"
+            onChange={handleChange}
+          />
+        </div>
         <button>Iniciar</button>
       </form>
     </>
