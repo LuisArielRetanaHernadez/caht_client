e75fd4ec5a09cfae74a949910f6470ac330d533b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-11-17 15:54:13 -0600

    inicializando el nameSpace en el server.js con sus names_spaces

--- src/Pages/Home/Home.page.jsx
+++ src/Pages/Home/Home.page.jsx
@@ -1,6 +1,14 @@
-
+import { io } from "socket.io-client"
 const Home = () => {
-
+  const socket = io("http://localhost:3000/chat")
+  socket.on("connect", () => {
+    console.log("connected")
+    socket.emit("message", 'hola')
+  })
+  socket.on("ping", () => {
+    console.log("pong")
+  })
+  console.log(socket)
   return (
     <>
       <h1>Home</h1>
