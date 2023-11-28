70ae0837130890c90e07488256bf2b1403ed3380
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-11-28 17:45:05 -0600

    creando un div para envolver el contenido con un posiiton relative y agregando el component Socials

--- src/Pages/Login/Login.pages.jsx
+++ src/Pages/Login/Login.pages.jsx
@@ -7,6 +7,7 @@ import { loginAsync } from "../../features/user/userSlice"
 // styles
 import "./Login.styles.css"
 import { Link } from "react-router-dom"
+import Socials from "../../components/socials/Socials"
 
 const Login = () => {
   const [values, setValues] = useState({
@@ -36,12 +37,12 @@ const Login = () => {
     <>
       <section className="container container--center">
         <form className="form form--shadow rounded-10p" onSubmit={handleSubmit}>
+        <div style={{position: "relative", width: "100%", height: "100%"}}>
           <h2 className="form__title">Login</h2>
 
           <div className="form__field
               form__field--medium
               form__field-center-m
-              position-relative
               form__field-mt-15p 
             ">
             <label className="form__field-title position-relative">Correo</label>
@@ -56,9 +57,8 @@ const Login = () => {
           <div className="form__field 
             form__field--medium
             form__field-center-m
-            position-relative
             form__field-mt-15p 
-           ">
+          ">
             <label className="form__field-title">Contraseña</label>
             <input 
               className="form__input form__input--color-blue"
@@ -68,7 +68,7 @@ const Login = () => {
             />
           </div>
 
-          <div className="form__buttons m-10p position-relative">
+          <div className="form__buttons m-10p">
             <button className="button button--hover-color-pear rounded-10p">
               Iniciar
             </button>
@@ -76,11 +76,16 @@ const Login = () => {
               Registrarse
             </Link>
           </div>
-          <div className="form__reset m-10p position-relative">
+
+          <div className="form__reset m-10p">
             <Link to="/forgot-password" className="form__link">
               Olvidaste tu contraseña?
             </Link>
           </div>
+
+          <Socials />
+        </div>
+         
         </form>
       </section>
     </>
