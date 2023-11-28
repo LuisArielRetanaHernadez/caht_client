4c33e63b5997d5ff1bad2cbf108201880fa6aed4
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-11-28 17:28:12 -0600

    creando el components Socials con el icon Google

---
+++ src/components/socials/Socials.jsx
@@ -0,0 +1,16 @@
+import Google from "../../assets/svg/google.svg"
+
+const Socials = () => {
+
+  return (
+    <>
+      <div>
+        <ul>
+          <li><Google /></li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Socials
