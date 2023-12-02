dd76a7463efe380523ce92df30f60b2946bd28c8
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-12-02 14:50:29 -0600

    agreganod condicion en el input password para cambiar su tipo a password a text o vicervesa

--- src/Pages/Login/Login.pages.jsx
+++ src/Pages/Login/Login.pages.jsx
@@ -22,7 +22,7 @@ const Login = () => {
     email: "",
     password: "",
   })
-  const [showPassword, setShowPassword] = useState(false)
+  const [showPassword, setShowPassword] = useState(true)
   const dispatch = useDispatch()
   
   const handleChange = (e) => {
@@ -50,7 +50,7 @@ const Login = () => {
     <>
       <section className="container container--center">
         <form className="form form--shadow rounded-10p" onSubmit={handleSubmit}>
-        <div className="text-center" style={{position: "relative", width: "100%", height: "100%", backdropFilter: blur("10px")}}>
+        <div className="text-center " style={{position: "relative", width: "100%", height: "100%", backdropFilter: blur("10px")}}>
           <h2 className="form__title">Login</h2>
 
           <div className="form__field
@@ -80,13 +80,13 @@ const Login = () => {
               onClick={togglePassword} 
               className="icon"
               >
-                {showPassword ? <img className="icon__img" src={closedEye} alt="closedEye" /> 
-                : <img className="icon__img" src={openEye} alt="openEye" /> }
+                {showPassword ? <img className="icon__img" src={openEye} alt="openEye" /> 
+                : <img className="icon__img" src={closedEye} alt="claseEye" /> }
               </span>
               <input 
                 className="form__input form__input--color-blue"
                 name="password"
-                type="password"
+                type={showPassword ? "password" : "text"}
                 onChange={handleChange}
               />
 
