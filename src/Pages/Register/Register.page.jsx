beb2505ccde1c5eea38f6b02c49cdfe24b045969
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-12-02 19:40:23 -0600

    creando el array dataFields con los inputs y labels

--- src/Pages/Register/Register.page.jsx
+++ src/Pages/Register/Register.page.jsx
@@ -1,8 +1,91 @@
 
 const Register = () => {
 
+  const datasFields= [
+    {
+      input: {
+        clasName: '',
+        id: '',
+        text: 'name',
+        type: 'text'
+      },
+      label: {
+        clasName: '',
+        id: '',
+        text: 'Name',
+        for: '',
+      }
+    },
+    {
+      input: {
+        clasName: '',
+        id: '',
+        text: 'Last Name',
+        type: 'text'
+      },
+      label: {
+        clasName: '',
+        id: '',
+        text: 'Last Name',
+        for: '',
+      }
+    },
+    {
+      input: {
+        clasName: '',
+        id: '',
+        text: 'Age',
+        type: 'number'
+      },
+      label: {
+        clasName: '',
+        id: '',
+        text: 'Age',
+        for: '',
+      }
+    },
+    {
+      input: {
+        clasName: '',
+        id: '',
+        text: 'Password',
+        type: 'password'
+      },
+      label: {
+        clasName: '',
+        id: '',
+        text: 'Password',
+        for: '',
+      }
+    },
+    {
+      input: {
+        clasName: '',
+        id: '',
+        text: 'Confirm Password',
+        type: 'password'
+      },
+      label: {
+        clasName: '',
+        id: '',
+        text: 'Confirm Password',
+        for: '',
+      }
+    },
+  ]
+
   return (
-    <div>Register</div>
+    <>
+      <section>
+        <form>
+          <div>
+            <div>
+              
+            </div>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
