48949ffaf339b506a7fa57e255c183d6beabba4e
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-12-26 15:30:36 -0600

    quitando la configuracion del io

--- src/Pages/Home/Home.page.jsx
+++ src/Pages/Home/Home.page.jsx
@@ -1,14 +1,13 @@
-import { io } from "socket.io-client"
+import manager from "../../utils/websocket"
+
+const socket = manager.socket('/users')
+
+socket.on('connect', () => {
+  console.log('connected')
+  socket.emit('message', 'hello')
+})
+
 const Home = () => {
-  const socket = io("http://localhost:3000/chat")
-  socket.on("connect", () => {
-    console.log("connected")
-    socket.emit("message", 'hola')
-  })
-  socket.on("ping", () => {
-    console.log("pong")
-  })
-  console.log(socket)
   return (
     <>
       <h1>Home</h1>
