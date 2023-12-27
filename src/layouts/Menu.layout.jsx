1f89876ef8d2b8c2aadcc06d4dadfee614989b95
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-12-27 17:31:30 -0600

    creando el menu header

---
+++ src/layouts/Menu.layout.jsx
@@ -0,0 +1,23 @@
+import Logo from "../components/Logo/Logo"
+
+
+const Menu = () => {
+
+
+  return (
+    <header>
+      <div>
+        <Logo />
+        <span>Pu</span>
+        <nav>
+          <ul>
+            <li><a href="/logout">Logout</a></li>
+            <li><a href="/login">Login</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Menu;
