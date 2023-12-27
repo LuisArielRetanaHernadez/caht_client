7fe362614d22fec2247c5e4418995ec733d0c226
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-12-26 18:56:09 -0600

    quitando los conosle.log

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,11 +1,12 @@
 import axios from './Axios';
 
 const login = (credentials) => {
-  console.log('credenciales ', credentials)
   return axios.post('/users/login', credentials)
   .then(res => {
-    localStorage.setItem('token', JSON.stringify(res.data.token))
-    return res
+    localStorage.setItem('token', JSON.stringify(res.data.data.token))
+    return res.data
+  }).catch(error => {
+    throw error
   })
 }
 
