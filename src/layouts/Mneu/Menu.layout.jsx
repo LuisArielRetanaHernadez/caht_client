4847509161a5e801425f54b419c6c6deef939b25
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-12-27 17:46:24 -0600

    agregando el componente Outlet de router dom y agregadno la importacion de Menu.layout.css

--- src/layouts/Mneu/Menu.layout.jsx
+++ src/layouts/Mneu/Menu.layout.jsx
@@ -1,22 +1,32 @@
+// componentes
 import Logo from "../../components/Logo/Logo"
 
+// router-dom-v6
+import { Outlet } from "react-router";
+
+// style
+import "./Menu.layout.css" 
 
 const Menu = () => {
 
 
   return (
-    <header>
-      <div>
-        <Logo />
-        <span>Pu</span>
-        <nav>
-          <ul>
-            <li><a href="/logout">Logout</a></li>
-            <li><a href="/login">Login</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+    <>
+      <header>
+        <div>
+          <Logo />
+          <span>Pu</span>
+          <nav>
+            <ul>
+              <li><a href="/logout">Logout</a></li>
+              <li><a href="/login">Login</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <Outlet />
+    </>
+
   )
 }
 
