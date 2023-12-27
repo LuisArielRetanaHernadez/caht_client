98c2043d94273d72b1b9d25c9e82e9fd99888cba
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-12-27 16:37:15 -0600

    creadno la funcion leadUserState donde nos devuelve los estados persistentes en el localStorage, agregando la propidad proloeadState con los objectos preloadedState y persisteState

--- src/app/store.js
+++ src/app/store.js
@@ -3,8 +3,34 @@ import { configureStore } from "@reduxjs/toolkit";
 // slices
 import userSlice from "../features/user/userSlice";
 
+const leadUserState = () => {
+    const serializedState = localStorage.getItem('user');
+    console.log(JSON.parse(serializedState));
+    if (serializedState === null) {
+        return undefined;
+    }
+    return JSON.parse(serializedState);
+}
+
+const preloadedState = {
+    user: { 
+        isLogin: false,
+        token: "",
+        user: {},
+        rol: "standar"
+    }
+}
+
+const persistedState = leadUserState();
+
+console.log(persistedState);
+
 export const store = configureStore({
     reducer: {
         user: userSlice.reducer
+    },
+    preloadedState: {
+        ...preloadedState,
+        user: persistedState,
     }
 })
