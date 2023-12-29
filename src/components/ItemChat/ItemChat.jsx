856911eb43fd0d1223a39786c59c45cafc6f47cd
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-12-28 20:41:41 -0600

    creando el componente donde rendirizar la vista previa de la lista de los contactos

---
+++ src/components/ItemChat/ItemChat.jsx
@@ -0,0 +1,21 @@
+const ItemChat = () => {
+
+  return (
+    <>
+      <li>
+        <div>
+          <figure>
+            <img src="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" alt="avatar" />
+          </figure>
+          <div>
+            <span>Name User</span>
+            <p>Send Message</p>
+          </div>
+        </div>
+      </li>
+
+    </>
+  )
+}
+
+export default ItemChat;
