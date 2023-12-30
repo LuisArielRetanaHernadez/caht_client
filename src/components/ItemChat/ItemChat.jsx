6da2d4e025cccd07b5df406650f2a7e38e511977
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-12-30 14:59:36 -0600

    agregando el componente Link y creando las clases elementos de la clase 'list-chat'

--- src/components/ItemChat/ItemChat.jsx
+++ src/components/ItemChat/ItemChat.jsx
@@ -1,20 +1,23 @@
+import { Link } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
 const ItemChat = ({name, messageLast, photo}) => {
 
   return (
     <>
-      <li>
-        <div>
-          <figure>
-            <img src={photo} alt="photo user" />
-          </figure>
-          <div>
-            <span>{name}</span>
-            <p>{messageLast}</p>
+      <li className="list-chat__item">
+        <Link className="list-chat__link">
+          <div className="list-chat__content">
+            <figure className="list-chat__image">
+              <img className="list-chat__img" src={photo} alt="photo user" />
+            </figure>
+            <div className="list-chat__information">
+              <span className="list-chat__name">{name}</span>
+              <p className="list-chat__message">{messageLast}</p>
+            </div>
           </div>
-        </div>
+        </Link>
       </li>
-
     </>
   )
 }
