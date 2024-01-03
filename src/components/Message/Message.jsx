38bedf2b65d847281083752be367ed19055b82c3
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-02 19:38:16 -0600

    creando las clases para el bloque 'message'

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -6,19 +6,19 @@ const Meesage = ({
 }) => {
     return (
         <>
-        <article>
-          <header>
-            <h4>{author}</h4>
-            <ul>
-              <li>
-                <a href="#">Edit</a>
+        <article className="message">
+          <header className="message__header">
+            <h4 className="message__name">{author}</h4>
+            <ul className="message__options">
+              <li className="message__option">
+                <button className="message__button" href="#">Edit</button>
               </li>
-              <li>
-                <a href="#">Delete</a>
+              <li className="message__option">
+                <button className="message__button" href="#">Delete</button>
               </li>
             </ul>
           </header>
-          <p>{message}</p>
+          <p className="message__content">{message}</p>
         </article>
         </>
     )
