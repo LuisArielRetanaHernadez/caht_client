f232af617c572540b2d22f67d653775eb360eb7d
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-03 18:34:22 -0600

    agregando la clase message--float-right' en el componente 'Message'

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -10,7 +10,7 @@ const Message = ({
 }) => {
     return (
         <>
-        <article className="message message--rounded-10px">
+        <article className="message message--rounded-10px message--float-right">
           <header className="message__header">
             <h4 className="message__name">{author}</h4>
             <ul className="message__options">
