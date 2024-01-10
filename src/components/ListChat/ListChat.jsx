aa16afb733797e73327c6f5c437656d14f7edd6b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-09 19:02:06 -0600

    quitando los console.log()

--- src/components/ListChat/ListChat.jsx
+++ src/components/ListChat/ListChat.jsx
@@ -4,30 +4,18 @@ import ItemChat from "../ItemChat/ItemChat"
 import './ListChat.css'
 
 // manager insta
-import manager from "../../utils/websocket"
 import { useState } from 'react'
 import Search from "../Search/Search"
 
 // axios
 import axios from "../../utils/axios"
 
-const socket = manager.socket("/users")
-
 const ListChat = () => {
   const [users, setUsers] = useState([])
 
-  socket.emit("list users", () => {
-    socket.on("list users", (data) => {
-      setUsers(data)
-    })
-  })
-
-
   const searchUsers = async (value) => {
-    const usersFinds = await axios.get(`/users/search
-    ?user=${value}`, { value })
+    const usersFinds = await axios.get(`/users/search?user=${value}`, { value })
     if (usersFinds) {
-      console.log('usuario encontrados ', usersFinds.data.data.usersFind)
       setUsers(usersFinds.data.data.usersFind)
     }
   }
