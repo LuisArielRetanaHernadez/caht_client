172fdb537f9cfa60da2e5d85f18b4ba15c353594
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-22 22:29:40 -0600

    forteando el archivo index.css

--- src/layouts/ListChat/ListChat.layout.jsx
+++ src/layouts/ListChat/ListChat.layout.jsx
@@ -3,9 +3,13 @@ import ListChat from "../../components/ListChat/ListChat";
 
 const ListChatLayout = () => {
   return (
-    <div className="">
-      <ListChat />
-      <Outlet /> 
+    <div className="wrapped wrapped--list-layout bg-gradient">
+      <div className="w-25">
+        <ListChat />
+      </div>
+      <div className="flex-1">
+        <Outlet /> 
+      </div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -15,8 +15,9 @@ import Login from './Pages/Login/Login.pages.jsx'
 import Register from './Pages/Register/Register.page.jsx'
 import Menu from './layouts/Mneu/Menu.layout.jsx'
 // import Home from './Pages/Home/Home.page.jsx'
-import Chat from './Pages/Chat/Chat.jsx'
-import ListChatLayout from './layouts/ListChat/ListChat.layout.jsx'
+// import Chat from './Pages/Chat/Chat.jsx'
+// import ListChatLayout from './layouts/ListChat/ListChat.layout.jsx'
+import ListChat from './components/ListChat/ListChat.jsx'
 
 
 
@@ -27,17 +28,17 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <ListChatLayout />,
-        children: [
-          {
-            path: '/chat/:id',
-            element: <Chat />,
-          },
-          {
-            path: '/perfil/:id',
-            element: <h1>Perfil</h1>
-          }
-        ]
+        element: <ListChat />,
+        // children: [
+        //   {
+        //     path: '/chat/:id',
+        //     element: <Chat />,
+        //   },
+        //   {
+        //     path: '/perfil/:id',
+        //     element: <h1>Perfil</h1>
+        //   }
+        // ]
       },
       {
         path: '/login',
