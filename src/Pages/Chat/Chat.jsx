48081f2803833d4aac1e143ebf2b080c1e87eddd
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-27 19:30:00 -0600

    creando un mesaje con el componente Message

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-
+import Message from "../../components/Message/Message"
 // style chat
 import "./Chat.style.css"
 const Chat = () => {
-  const [message, setMessage] = useState([{}])
+  const [message, setMessage] = useState([{
+    id: 1,
+    message: 'Hola como estas',
+    author: 'luis'
+  }])
   const params = useParams()
   const { id } = params
 
@@ -22,9 +26,9 @@ const Chat = () => {
   return (
     <div className="chat">
       <div className="chat__messages">
-        {/* {message.map((m) => (    
-          <div key={m.id}>{m.message}</div>
-        )) ?? "No hay mensajes"} */}
+        {message.map((m) => (    
+          <Message key={m.id} message={m.message} author={m.author} />
+        )) ?? "No hay mensajes"}
       </div>
       <div className="chat__box-send-message">
         <form className="chat__form rounded-10px" action="">
