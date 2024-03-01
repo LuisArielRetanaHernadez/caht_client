8649cc389026a21d499983ea6ecfe7aa29d5b861
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-29 19:09:31 -0600

    creando la clase 'user-preview--center'

--- src/components/userPreview/UserPreview.jsx
+++ src/components/userPreview/UserPreview.jsx
@@ -1,7 +1,7 @@
 import './UserPreview.css'
 const UserPreview = () => {
   return (
-    <div className="user-preview user-preview--gap-8 p-1rem rounded-10">
+    <div className="user-preview user-preview--center-content user-preview--gap-8 p-1rem rounded-10">
       <div className="avatar">
         <img
         className="avatar__img"
