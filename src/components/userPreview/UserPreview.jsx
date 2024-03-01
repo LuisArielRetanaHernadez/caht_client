33606253b9551e3b1afa791f049bb4aa861b2b0b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-29 18:23:32 -0600

    creando el componente UserPreview

---
+++ src/components/userPreview/UserPreview.jsx
@@ -0,0 +1,25 @@
+const UserPreview = () => {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <p>Name</p>
+        <p>Amigo</p>
+      </div>
+      <div>
+      <span>
+        mas
+      </span>
+        <ul>
+          <li>Bloquear</li>
+          <li>Agregar</li>
+          <li>Editar</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default UserPreview
