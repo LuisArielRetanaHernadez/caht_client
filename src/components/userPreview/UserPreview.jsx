39c35ea53fee9b34fc10d3bb05c8c2a9d9353dab
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-29 20:22:52 -0600

    creando la clase 'icon--hover-white'

--- src/components/userPreview/UserPreview.jsx
+++ src/components/userPreview/UserPreview.jsx
@@ -16,7 +16,7 @@ const UserPreview = () => {
         <p className="user-preview__frende">Amigo</p>
       </div>
       <div>
-      <span className="icon pointer">
+      <span className="icon icon--hover-white pointer">
       <FontAwesomeIcon icon={faArrowDown} />
       </span>
         <ul className='list list--sub'>
