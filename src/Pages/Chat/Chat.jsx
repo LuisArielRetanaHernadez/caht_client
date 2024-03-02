75d05c2e82dbd7d7e20f2cd1361ebafd0133bc75
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-01 23:00:21 -0600

    crendo la funcionalidad para enviar mensajes

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -5,25 +5,50 @@ import Message from "../../components/Message/Message"
 import "./Chat.style.css"
 import { useSelector } from "react-redux"
 import UserPreview from "../../components/userPreview/UserPreview"
+// import axios from "axios"
+import instanceAxios from "../../utils/axios"
+import manager from "../../utils/websocket"
 const Chat = () => {
-  const [message, setMessage] = useState([{
-    id: 1,
-    message: 'Hola como estas',
-    author: 'luis'
-  }])
-  const params = useParams()
-  const { id } = params
+  const socket = manager.socket('/users')
+  const [messages, setMessages] = useState([])
+  const [message, setMessage] = useState('')
 
+  const { id } = useParams()
 
   useEffect(() => {
 
     const getMessage = async () => {
-      const response = await fetch(`XXXXXXXXXXXXXXXXXXXXXXXXXXX${id}`)
-      const data = await response.json()
-      setMessage(data)
+      instanceAxios.get(`/messages/${id}`)
+        .then((res) => {
+          console.log(res.data)
+          setMessages(res.data)
+        })
     }
     getMessage()
   },[])
+
+  useEffect(() => {
+    socket.on('message', (data) => {
+      console.log(data)
+      setMessages(prev => [...prev, data])
+    })
+  }, [socket])
+
+  const sendMessage = (e) => {
+    e.preventDefault()
+    // socket.emit('message', message)
+    // console.log(message)
+    // setMessage(prev => [...prev, message])
+    // console.log(message)
+    // axios.post('/messages', message)
+    //   .then((res) => {
+    //     console.log(res.data)
+    //   })
+    //   .catch((err) => {
+    //     console.log(err)
+    //   })
+    socket.emit('send message', messages)
+  }
 
   const { isLogin } = useSelector((state) => state.user);
 
@@ -35,17 +60,19 @@ const Chat = () => {
     <div className="chat">
       <div className="chat__messages">
       <UserPreview />
-        {message.map((m) => (    
+        {messages.map((m) => (    
           <Message key={m.id} message={m.message} author={m.author} />
         )) ?? "No hay mensajes"}
       </div>
       <div className="chat__box-send-message">
-        <form className="chat__form rounded-10px" action="">
+        <form onSubmit={sendMessage} className="chat__form rounded-10px" action="">
           <textarea
             rows={2}
             wrap="hard"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
            className="input input--send-message rounded-10" type="text" />
-          <button className="button button--send-message rounded-10">Enviar</button>
+          <buttonm className="button button--send-message rounded-10">Enviar</buttonm>
         </form> 
       </div>
     </div>
