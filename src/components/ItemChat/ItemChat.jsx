282afbc73eccd576e84e85fa630f781e079d069a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-04 19:35:21 -0600

    formateando el codigo del componente ItemChat.jsx

--- src/components/ItemChat/ItemChat.jsx
+++ src/components/ItemChat/ItemChat.jsx
@@ -1,20 +1,28 @@
-import { Link } from "react-router-dom";
+/* eslint-disable react/prop-types */
 
-import '../ListChat/ListChat.css'
-import manager from "../../utils/websocket";
+// react hooks
 import { useEffect, useState } from 'react';
 
-/* eslint-disable react/prop-types */
+// router-dom
+import { Link } from "react-router-dom";
+
+// manager socket
+import manager from "../../utils/websocket";
+
+// style ListChat.css
+import '../ListChat/ListChat.css'
+
 const ItemChat = ({ id, name, messageLast, photo}) => {
   const [userOnline, setUserOnline] = useState(false)
   const socket = manager.socket('/users')
 
   useEffect(() => {
-    socket.emit('users online')
     socket.on('users online', (data) => {
+
       const isOnline = data.some(user => {
-        return id === user._id;  // id === user.userId ? true : false;  // id === user.userId ? true : false;  // id === user.userId ? true : false;  // id === user.userId ? true : false;  // id === user.userId ?
+        return id === user._id;
       })
+
       setUserOnline(isOnline)
     })
 
@@ -23,10 +31,6 @@ const ItemChat = ({ id, name, messageLast, photo}) => {
     }
   },[id])
 
-  useEffect(() => {
-    console.log('userOnline ', userOnline)
-  }, [userOnline])
-
 
   return (
     <>
@@ -34,13 +38,16 @@ const ItemChat = ({ id, name, messageLast, photo}) => {
         <Link 
         to={`/chat/${id}`}
         className="list-chat__link">
+
           <figure className={`${photo ? '' : 'bg-gradient'} list-chat__image`}>
             {photo ? <img className="list-chat__img" src={photo} alt="photo user" /> : ''}
           </figure>
+
           <div className="list-chat__information">
             <span className="list-chat__name">{name}</span>
             <p className="list-chat__message">{messageLast}</p>
           </div>
+
         </Link>
 
         <span className={`circle circle--small circle--0-tr ${userOnline ? 'circle--online' : 'circle--offline'}`}>
