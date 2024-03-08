b481aaf42a678d15c0a07ae48e8a0b4371e92864
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-08 17:53:57 -0600

    el compoenente mobal

---
+++ src/components/Mobal/Mobal.jsx
@@ -0,0 +1,15 @@
+const Mobal = () => {
+
+  return (
+    <div>
+      <h2>Title mobal</h2>
+      <p>Content</p>
+      <div>
+        <button>Close</button>
+        <button>Acepta</button>
+      </div>
+    </div>
+  )
+}
+
+export default Mobal
