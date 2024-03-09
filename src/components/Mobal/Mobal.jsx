472a308baefca8fe06ebca5052ed3a59c36ed6f1
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-08 19:21:06 -0600

    agregamos las clase 'py-5' y 'px-5'

--- src/components/Mobal/Mobal.jsx
+++ src/components/Mobal/Mobal.jsx
@@ -7,7 +7,7 @@ const Mobal = () => {
       <h2 className="mobal__title pb-5">Title mobal</h2>
       <p className="mobal__text pb-5">Content</p>
       <div className="mobal__buttons pb-5">
-        <button className="button button--white">Close</button>
+        <button className="button button--white px-5 py-5">Close</button>
         <button className="button">Acepta</button>
       </div>
     </div>
