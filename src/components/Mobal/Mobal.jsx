ca470927d4d4664f1953a2df688603381a2125ba
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-08 18:14:47 -0600

    agregando las clase 'text-center' al contendor mobal y la clase 'button--white' al boton close

--- src/components/Mobal/Mobal.jsx
+++ src/components/Mobal/Mobal.jsx
@@ -3,11 +3,11 @@ import './Mobal.css'
 const Mobal = () => {
 
   return (
-    <div className="mobal p-1rem rounded-10">
+    <div className="mobal text-center p-1rem rounded-10">
       <h2 className="mobal__title">Title mobal</h2>
       <p className="mobal__text">Content</p>
       <div className="mobal__buttons">
-        <button className="button">Close</button>
+        <button className="button button--white">Close</button>
         <button className="button">Acepta</button>
       </div>
     </div>
