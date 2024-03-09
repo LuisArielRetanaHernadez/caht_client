97dd88e6454d817d002afeb8fab82e3b2d590fe1
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-08 19:00:47 -0600

    agregamos la clase 'p-5' y 'pb-5'

--- src/components/Mobal/Mobal.jsx
+++ src/components/Mobal/Mobal.jsx
@@ -3,10 +3,10 @@ import './Mobal.css'
 const Mobal = () => {
 
   return (
-    <div className="mobal text-center p-1rem rounded-10">
-      <h2 className="mobal__title">Title mobal</h2>
-      <p className="mobal__text">Content</p>
-      <div className="mobal__buttons">
+    <div className="mobal text-center p-5 rounded-10">
+      <h2 className="mobal__title pb-5">Title mobal</h2>
+      <p className="mobal__text pb-5">Content</p>
+      <div className="mobal__buttons pb-5">
         <button className="button button--white">Close</button>
         <button className="button">Acepta</button>
       </div>
