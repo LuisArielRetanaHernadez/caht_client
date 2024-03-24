fad00b0a82be7f51d7837d4acc0dca007f7f068d
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-23 18:07:37 -0600

    creando la funcialidad para ocultar o mostrar el mobal de error

--- src/components/Mobal/Mobal.jsx
+++ src/components/Mobal/Mobal.jsx
@@ -1,28 +1,45 @@
 import { useEffect, useRef, useState } from 'react'
 import './Mobal.css'
 
+// redux
+import { useDispatch, useSelector } from 'react-redux'
+import { clearError } from '../../features/error/errorSlice'
+
 const Mobal = () => {
   const [close, setClose] = useState(false)
 
   const mobal = useRef(null)
 
+  const dispatch = useDispatch()
+
+  const {
+    message,
+    status,
+    isError
+  } = useSelector(state => state.error)
+
   useEffect(() => {
-    if (mobal.current) {
-      if (close) {
+    closeMobal()
+  }, [])
+
+  const closeMobal = () => {
+        if (mobal.current) {
+      if (isError) {
         mobal.current.classList.add('mobal--close')
       } else {
         mobal.current.classList.remove('mobal--close')
+        dispatch(clearError)
       }
     }
-  }, [close])
+  } 
 
   return (
     <div ref={mobal} className="mobal text-center p-5 rounded-10">
-      <h2 className="mobal__title pb-5">Title mobal</h2>
-      <p className="mobal__text pb-5">Content</p>
+      <h2 className="mobal__title pb-5">{status ?? 'error'}</h2>
+      <p className="mobal__text pb-5">{message}</p>
       <div className="mobal__buttons pb-5">
         <button
-          onClick={() => setClose(true)}
+          onClick={() => closeMobal()}
          className="button button--white px-5 py-5">Close</button>
         <button className="button button--gray-teel px-5 py-5">Acepta</button>
       </div>
