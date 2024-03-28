c697463e637d4d1fe55da5fd89f6bb93abb2d5e3
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-28 16:45:17 -0600

    crenado la funcionalidad para ocultar la lista de chat en el layout cuando el viewport es menor a 768px

--- src/layouts/ListChat/ListChat.layout.jsx
+++ src/layouts/ListChat/ListChat.layout.jsx
@@ -1,20 +1,40 @@
-import { Navigate, Outlet } from "react-router-dom";
+/* eslint-disable react-hooks/rules-of-hooks */
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import ListChat from "../../components/ListChat/ListChat";
 import "./ListChat.style.css";
 import { useSelector } from 'react-redux';
-
+import { useEffect } from "react";
+import { useState } from 'react';
 
 
 const ListChatLayout = () => {
+  const [showListChat, setShowListChat] = useState(true);
 
   const { isLogin } = useSelector((state) => state.user);
+
+  let location = useLocation();
 
   if (!isLogin) {
     return <Navigate to="/login" />
   }
+
+  useEffect(() => {
+    const widthViewport = window.innerWidth;
+    if (widthViewport < 768) {
+      if (location.pathname !== "/") {
+        setShowListChat(false);
+      }
+    }
+
+    return () => {
+      setShowListChat(true)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps 
+  }, [location, window.innerWidth])
+
   return (
     <div className="layout-list-chat wrapped wrapped--menu">
-      <div className="layout-list-chat--list w-25 h-full">
+      <div className={`layout-list-chat--list ${!showListChat ? 'layout-list-chat--hidden' : ''} w-25 h-full`}>
         <ListChat />
       </div>
       <div className="layout-list-chat--content">
