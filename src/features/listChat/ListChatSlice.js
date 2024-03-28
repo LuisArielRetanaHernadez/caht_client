f81289547b0c39d46a13eb97f7bdf8c5504e93f8
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-28 17:15:46 -0600

    creando el slice ListChatSlice con un reducer toggleVisibility

---
+++ src/features/listChat/ListChatSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  chats: [],
+  isShow: false,
+}
+
+const listChatSlice = createSlice({
+  name: "listChat",
+  initialState,
+
+  reducers: {
+    toggleVisibility(state) {
+      state.isShow = !state.isShow
+    }
+  }
+})
+
+export const { toggleVisibility } = listChatSlice.actions
+
+export default initialState
