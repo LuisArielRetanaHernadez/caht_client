7df985bc60d709f39b5dd672f59393d0a439ab19
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-28 18:38:58 -0600

    quitnado el boton para mostrar/ocultar la lista de chats

--- src/layouts/ListChat/ListChat.layout.jsx
+++ src/layouts/ListChat/ListChat.layout.jsx
@@ -2,34 +2,20 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import ListChat from "../../components/ListChat/ListChat";
 import "./ListChat.style.css";
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useEffect } from "react";
 import { useState } from 'react';
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { toggleVisibility } from "../../features/listChat/ListChatSlice";
-
 
 const ListChatLayout = () => {
   const [showListChat, setShowListChat] = useState(true);
 
-  const dispatch = useDispatch();
-
   const { isLogin } = useSelector((state) => state.user);
-
-  const isShowListChat = useSelector(state => state.listChat.isShow)
 
   let location = useLocation();
 
   if (!isLogin) {
     return <Navigate to="/login" />
   }
-
-  const toggleListChat = () => {
-    dispatch(toggleVisibility())
-  }
-
 
   useEffect(() => {
     const widthViewport = window.innerWidth;
@@ -49,14 +35,6 @@ const ListChatLayout = () => {
     <div className="layout-list-chat wrapped wrapped--menu">
       <div className={`layout-list-chat--list ${!showListChat ? 'layout-list-chat--hidden' : ''} w-25 h-full`}>
         <ListChat />
-        <span className="button button--list-chat text-center"
-        onClick={toggleListChat}
-        >
-        {
-          isShowListChat ? <FontAwesomeIcon icon={faArrowRight} />
-          : <FontAwesomeIcon icon={faArrowLeft} />
-        }
-        </span>
       </div>
       <div className="layout-list-chat--content">
         <Outlet /> 
