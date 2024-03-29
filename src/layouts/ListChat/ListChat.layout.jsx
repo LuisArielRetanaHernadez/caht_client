fae3416c309ab7ac6941eb8921131acfc2467152
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-28 18:04:25 -0600

    creando la funcionalidad solo para cambiar la direcion de la flecha del boton para mostrar/ocultar la lista de chats

--- src/layouts/ListChat/ListChat.layout.jsx
+++ src/layouts/ListChat/ListChat.layout.jsx
@@ -2,24 +2,34 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import ListChat from "../../components/ListChat/ListChat";
 import "./ListChat.style.css";
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from "react";
 import { useState } from 'react';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { toggleVisibility } from "../../features/listChat/ListChatSlice";
 
 
 const ListChatLayout = () => {
   const [showListChat, setShowListChat] = useState(true);
 
+  const dispatch = useDispatch();
+
   const { isLogin } = useSelector((state) => state.user);
+
+  const isShowListChat = useSelector(state => state.listChat.isShow)
 
   let location = useLocation();
 
   if (!isLogin) {
     return <Navigate to="/login" />
   }
+
+  const toggleListChat = () => {
+    dispatch(toggleVisibility())
+  }
+
 
   useEffect(() => {
     const widthViewport = window.innerWidth;
@@ -39,8 +49,13 @@ const ListChatLayout = () => {
     <div className="layout-list-chat wrapped wrapped--menu">
       <div className={`layout-list-chat--list ${!showListChat ? 'layout-list-chat--hidden' : ''} w-25 h-full`}>
         <ListChat />
-        <span className="button button--list-chat text-center">
-          <FontAwesomeIcon icon={faArrowLeft} />
+        <span className="button button--list-chat text-center"
+        onClick={toggleListChat}
+        >
+        {
+          isShowListChat ? <FontAwesomeIcon icon={faArrowRight} />
+          : <FontAwesomeIcon icon={faArrowLeft} />
+        }
         </span>
       </div>
       <div className="layout-list-chat--content">
