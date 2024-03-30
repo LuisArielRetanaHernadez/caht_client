e6472b568fe7bffcef91834175d8c749dc47dbbb
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-30 17:28:01 -0600

    creando la pagina para verificar el correo registrado

---
+++ src/Pages/CheckEmail/CheckEmail.jsx
@@ -0,0 +1,15 @@
+const CheckEmail = () => {
+  return (
+    <div>
+      <h2>Verificacion del menu</h2>
+      <p>
+        Introduce el codigo que te enviamos al correo con el que te registrates
+      </p>
+      <form>
+        <input type="text" />
+      </form>
+    </div>
+  )
+}
+
+export default CheckEmail
