eb7fda19fbaca9255a5ba4860c6ced913ce3f78a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-30 18:49:31 -0600

    envolviendo el card dentro de un div para centra el card en el vierwport de la pagina

--- src/Pages/CheckEmail/CheckEmail.jsx
+++ src/Pages/CheckEmail/CheckEmail.jsx
@@ -1,15 +1,17 @@
 import './CheckEmail.css'
 const CheckEmail = () => {
   return (
-    <div className="card">
-      <h2 className="card__title">Verificacion del Correo</h2>
-      <p className="card__text">
-        Introduce el codigo que te enviamos al correo con el que te registrates
-      </p>
-      <form className="card__form">
-        <input className="input input--card" type="text" />
-      </form>
-    </div>
+    <section className='wrapped--menu container container--center'>
+      <div className="card">
+        <h2 className="card__title">Verificacion del Correo</h2>
+        <p className="card__text">
+          Introduce el codigo que te enviamos al correo con el que te registrates
+        </p>
+        <form className="card__form">
+          <input className="input input--card" type="text" />
+        </form>
+      </div>
+    </section>
   )
 }
 
