a16c2d77cd69e32033297167c927184be5685aa9
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-30 18:43:53 -0600

    corregiendo el titulo a 'verificacion del Correo

--- src/Pages/CheckEmail/CheckEmail.jsx
+++ src/Pages/CheckEmail/CheckEmail.jsx
@@ -2,7 +2,7 @@ import './CheckEmail.css'
 const CheckEmail = () => {
   return (
     <div className="card">
-      <h2 className="card__title">Verificacion del menu</h2>
+      <h2 className="card__title">Verificacion del Correo</h2>
       <p className="card__text">
         Introduce el codigo que te enviamos al correo con el que te registrates
       </p>
