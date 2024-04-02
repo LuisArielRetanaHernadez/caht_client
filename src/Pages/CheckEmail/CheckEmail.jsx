e60f35bffa08750e586682e5d4cd3fdc815a1e38
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-02 15:59:59 -0600

    creando la funcionalidad para verificar el correo

--- src/Pages/CheckEmail/CheckEmail.jsx
+++ src/Pages/CheckEmail/CheckEmail.jsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from 'react';
 import './CheckEmail.css'
-import { useParams } from 'react-router-dom'
+import { Navigate, useParams } from 'react-router-dom'
+
+import Axios from '../../utils/axios'
 const CheckEmail = () => {
-  const { toke } = useParams();
+  const [code, setCode] = useState(null)
+  const [isVerify, setIsVerify] = useState(false)
+
+  const { token } = useParams();
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        await Axios.post(`/users/verify/email/token/${token}`)
+      } catch (error) {
+        return error
+      }
+    }
+    verifyToken()
+  },[token])
+
+  const verifyEmail = async () => {
+    try {
+      await Axios.post(`/users/check/email/${token}`, {
+        code
+      })
+      setIsVerify(true)
+    } catch (error) {
+      setIsVerify(false)
+      return error
+    }
+  }
+
+  useEffect(() => {
+    if (isVerify) {
+
+      return <Navigate to='/' />
+    }
+  },[isVerify])
+
   return (
     <section className='wrapped--menu container container--center'>
       <div className="card">
@@ -9,8 +46,11 @@ const CheckEmail = () => {
         <p className="card__text">
           Introduce el codigo que te enviamos al correo con el que te registrates
         </p>
-        <form className="card__form">
-          <input className="input input--card" type="text" />
+        <form className="card__form" onSubmit={verifyEmail}>
+          <input className="input input--card" type="text"
+            placeholder="XXXX"
+            onChange={(e) => setCode(e.target.value)}
+           />
         </form>
       </div>
     </section>
