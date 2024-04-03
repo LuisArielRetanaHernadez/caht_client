b2d1993ef9c45a3cd3c94d2e95e344d9ffa037dd
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-02 20:53:39 -0600

    quitando el parametro email y del body de la peticion en el enpoint para renviar un nuevo codigo al correo para verificarlo

--- src/utils/api/user.js
+++ src/utils/api/user.js
@@ -1,10 +1,8 @@
 import Axios from '../axios'
 
-export const resendCodeEmail = async (email, token) => {
+export const resendCodeEmail = async (token) => {
   try {
-    const resendCode = await Axios.get(`/resend/code/email/$${token}`, {
-      email
-    })
+    const resendCode = await Axios.get(`/resend/code/email/$${token}`)
     return resendCode.data
   } catch (error) {
     throw new Error(error.message)
