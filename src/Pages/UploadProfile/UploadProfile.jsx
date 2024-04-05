d272f0b2739ae9048f3900f9d8c1399ef98d28b0
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-04 19:30:29 -0600

    creando el componente/page UploadProfile para cargar la imagne del perfil

---
+++ src/Pages/UploadProfile/UploadProfile.jsx
@@ -0,0 +1,28 @@
+import CloudinaryWidget from "../../components/widgetCloudinary/CloudinaryWidget"
+import { useState } from 'react'
+import cld from "../../utils/cloudinary/cloudinary"
+import { AdvancedImage, placeholder, responsive } from "@cloudinary/react"
+
+const UploadProfile = () => {
+  const [publicId, setPublicId] = useState('')
+
+  return (
+    <section>
+      <div>
+        <h2>Upload Profile</h2>
+        <div>
+          <AdvancedImage
+            style={{ width: "100px", height: "100px" }}
+            cldImg={cld.image(publicId)}
+            plugins={[responsive(), placeholder()]}
+           />
+        </div>
+        <form>
+          <CloudinaryWidget uwConfig={'dqmkovsdy'} setPublicId={setPublicId}/>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default UploadProfile
