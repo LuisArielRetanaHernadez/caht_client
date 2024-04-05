ec29b35e73edddac76785e4385cd9d61eda8defa
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-05 14:23:37 -0600

    centado el contenido de la pagina y ajustando la imagen cargada

--- src/Pages/UploadProfile/UploadProfile.jsx
+++ src/Pages/UploadProfile/UploadProfile.jsx
@@ -7,12 +7,12 @@ const UploadProfile = () => {
   const [publicId, setPublicId] = useState('')
 
   return (
-    <section>
+    <section className="wrapped wrapped--menu-min-h wrapped--flex-center">
       <div>
         <h2>Upload Profile</h2>
         <div>
           <AdvancedImage
-            style={{ width: "100px", height: "100px" }}
+            style={{ width: "100px", height: "100px", objectFit: "cover" }}
             cldImg={cld.image(publicId)}
             plugins={[responsive(), placeholder()]}
            />
