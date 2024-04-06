8ccc0e495f1409b94acf3641f2b16989512a249a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-06 15:46:51 -0600

    corregiendo el endpoint para la verificacion del usuario por id y autentificacion

--- src/utils/api/user.js
+++ src/utils/api/user.js
@@ -20,7 +20,7 @@ export const uploadImageProfile = async (data) => {
 
 export const verifyUserByIdAndAuth = async (id) => {
   try {
-    const verifyUser = await Axios.get(`/users/verify/${id}`)
+    const verifyUser = await Axios.get(`/users/verify/user/${id}`)
     return verifyUser.data
   } catch (error) {
     return error
