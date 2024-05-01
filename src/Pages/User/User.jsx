749e5c8a38b7dfb140c6f969efc7dec8dfa7c6ab
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-30 19:40:39 -0600

    creando el componente (page) User

---
+++ src/Pages/User/User.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom"
+
+/* eslint-disable no-unused-vars */
+const User = (props) => {
+	return (
+		<div>
+			<header>
+        <div>
+          <img></img>
+        </div>
+        <div>
+          <img></img>
+        </div>
+      </header>
+
+      <div>
+        <div>
+          <label>Username</label>
+          <input></input>
+        </div>
+        <div>
+          <label>Name</label>
+          <input></input>
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input></input>
+        </div>
+      </div>
+
+      <div>
+      <h3>Contacts</h3>
+        <ul>
+          <li>
+            <Link >
+              <div>
+                <div>
+                  <img></img>
+                </div>
+              </div>
+              <div>
+                <h3>Name</h3>
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </div>
+		</div>
+  )
+}
+
+export default User
