fd510aac21e7ca0f8363c15e8e318c147ddafa87
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-30 23:02:34 -0600

    agregando clases a los elementos

--- src/Pages/User/User.jsx
+++ src/Pages/User/User.jsx
@@ -35,55 +35,57 @@ const User = (props) => {
     }
   }, [id])
 	return (
-		<div>
-			<header>
-        <div>
+		<div className="container">
+			<header className="header">
+        <div className="profile__image-header">
           <img></img>
         </div>
-        <div>
+        <div className="profile__image-user">
           <img></img>
         </div>
       </header>
 
-      <div>
-        <div>
-          <label>Username</label>
-          {
-            isAuthor ? 
-            <input type="text" placeholder="Username" defaultValue={'Username'} /> 
-            : <p>Username</p>
-          }
-        </div>
-        <div>
-          <label>Name</label>
-          {
-            isAuthor ?
-            <input type="text" placeholder="Name" defaultValue={'Name'} /> 
-            : <p>Name</p>
-          }
-        </div>
-        <div>
-          <label>Last Name</label>
-          {
-            isAuthor ? 
-            <input type="text" placeholder="Last Name" defaultValue={'Last Name'} /> 
-            : <p>Last Name</p>
-          }
-        </div>
+      <div className="profile_content">
+        <form className="form">
+          <div className="form__field">
+            <label className="form__label">Username</label>
+            {
+              isAuthor ? 
+              <input className="form__input" type="text" placeholder="Username" defaultValue={'Username'} /> 
+              : <p className="text">Username</p>
+            }
+          </div>
+          <div className="form__field">
+            <label className="form__label">Name</label>
+            {
+              isAuthor ?
+              <input className="form__input" type="text" placeholder="Name" defaultValue={'Name'} /> 
+              : <p className="text">Name</p>
+            }
+          </div>
+          <div className="form__field">
+            <label className="form__label">Last Name</label>
+            {
+              isAuthor ? 
+              <input className="form__input" type="text" placeholder="Last Name" defaultValue={'Last Name'} /> 
+              : <p className="text">Last Name</p>
+            }
+          </div>
+        </form>
       </div>
 
-      <div>
-      <h3>Contacts</h3>
-        <ul>
-          <li>
-            <Link >
-              <div>
-                <div>
+      <div className="profile_content">
+        <h3 className="sub-title">Contacts</h3>
+        <ul className="list">
+          <li className="item">
+            <Link className="user-content">
+              <div className="user-content__box">
+                <div className="user-content__avatar">
                   <img></img>
                 </div>
               </div>
-              <div>
-                <h3>Name</h3>
+              <div className="user-content__box">
+                <h3 className="text">Name</h3>
               </div>
             </Link>
           </li>
