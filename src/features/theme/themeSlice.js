50f3d321ce592c86a99fbca87c46d67856266219
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-05-03 15:30:02 -0600

    creando el slice theme con el reducer setBackgroundImage

---
+++ src/features/theme/themeSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+const initialState = {
+  backgroundImage: null
+}
+
+const themeSlice = createSlice({
+  name: 'theme',
+  initialState,
+  reducers: {
+    setBackgroundImage: (state, action) => {
+      state.backgroundImage = action.payload
+    }
+  }
+})
+
+export const { setBackgroundImage } = themeSlice.actions
+export default themeSlice
